5f5226eeae76399ec408a33cca4ea8f6e9f4b5b7
Hugo da Silva Alves <65432321+hugo96x@users.noreply.github.com>
2021-04-08 17:49:47 -0300

    :package: conta de orcamento

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ var hamburguer = document.querySelector(".hamburguer");
 
     });
     
+    document.querySelector("#qtd").addEventListener("change", atualizarPreco)
+    document.querySelector("#js").addEventListener("change", atualizarPreco)
+    document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+    document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+    document.querySelector("#prazo").addEventListener("change", function () {
+        const prazo = document.querySelector("#prazo").value
+        document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+        atualizarPreco()
+    })
+    
+    function atualizarPreco(){
+        const qtd    = document.querySelector("#qtd").value
+        const temJS = document.querySelector("#js").checked
+        const incluiLayout = document.querySelector("#layout-sim").checked
+        const prazo = document.querySelector("#prazo").value
+      
+        let preco = qtd * 100;
+        if (temJS) preco *= 1.1
+        if (incluiLayout) preco += 500
+        let taxaUrgencia = 1 - prazo*0.1;
+        preco *= 1 + taxaUrgencia
+       
+        document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+    }
+    
